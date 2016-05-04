9cc451979c52c8c86e34c1fff56aa35f13b0a062
zxldev <zxldev@live.com>
2016-05-04 11:21:04 +0800

    fix bug

--- public/js/main.js
+++ public/js/main.js
@@ -14,7 +14,7 @@ require.config({
         domready:'/js/domready',
         bootstrap:'/js/lib/bootstrap.min',
         tokenfield:['http://cdn.bootcss.com/bootstrap-tokenfield/0.12.0/bootstrap-tokenfield.min','/js/lib/bootstrap-tokenfield.min'],
-        infintescroll:['http://apps.bdimg.com/libs/jquery.infinitescroll.js/2.0.2/jquery.infinitescroll.min','/js/lib/jquery.infinitescroll']
+        infintescroll:['http://apps.bdimg.com/libs/jquery.infinitescroll.js/2.0.2/jquery.infinitescroll','/js/lib/jquery.infinitescroll']
     }
 });
 
